0ace69bda3738b8347d6df9741b127d303871561
volodymyr.balashov <volodymyr.balashov@glpgs.com>
2024-07-01 09:09:07 +0900

    add animation

--- main.js
+++ main.js
@@ -17,3 +17,43 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', checkReveal);
     checkReveal(); // Initial check in case some elements are already in view
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const reveals = document.querySelectorAll('.reveal-left');
+
+    function checkReveal() {
+        const windowHeight = window.innerHeight;
+        reveals.forEach(reveal => {
+            const revealTop = reveal.getBoundingClientRect().top;
+            const revealPoint = 150; // Adjust this value to control when the text appears
+
+            if (revealTop < windowHeight - revealPoint) {
+                reveal.classList.add('active');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', checkReveal);
+    checkReveal(); // Initial check in case some elements are already in view
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const reveals = document.querySelectorAll('.reveal-right');
+
+    function checkReveal() {
+        const windowHeight = window.innerHeight;
+        reveals.forEach(reveal => {
+            const revealTop = reveal.getBoundingClientRect().top;
+            const revealPoint = 150; // Adjust this value to control when the text appears
+
+            if (revealTop < windowHeight - revealPoint) {
+                reveal.classList.add('active');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', checkReveal);
+    checkReveal(); // Initial check in case some elements are already in view
+});
